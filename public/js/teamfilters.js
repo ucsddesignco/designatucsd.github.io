bcc2484e0f246228ed6d016a4ad9c3188a43d7d3
Enyu Chen <enyuchen123@gmail.com>
2019-07-15 19:25:17 -0700

    updated board members

--- public/js/teamfilters.js
+++ public/js/teamfilters.js
@@ -7,6 +7,127 @@ const GRAD_YEAR = 2016;
 // List of teams
 var team = [
   {
+    id: "katya",
+    name: "Katya Noble",
+    role: "Director of Growth and Strategy",
+    link: "https://www.katyanoble.design/",
+    gradYear: 2019
+  },
+  {
+    id: "emily",
+    name: "Emily Nguyen",
+    role: "Co-Creative Director",
+    link: "http://emilynguyen.co/",
+    gradYear: 2019
+  },
+  {
+    id: "lucy",
+    name: "Lucy Xu",
+    role: "Co-Creative Director",
+    link: "https://lucylouxu.com/",
+    gradYear: 2020
+  },
+  {
+    id: "nathan",
+    name: "Nathan Mansur",
+    role: "Director of Marketing and Operations",
+    link: "https://www.linkedin.com/in/nathan-mansur/",
+    gradYear: 2019
+  },
+  {
+    id: "steven",
+    name: "Steven Chen",
+    role: "Director of Industry Relations",
+    link: "https://www.stevenychen.com/",
+    gradYear: 2020
+  },
+  {
+    id: "jamin",
+    name: "Jamin Capulong",
+    role: "Community Engagement Coordinator",
+    link: "https://www.linkedin.com/in/jamin-nick-capulong/",
+    gradYear: 2021
+  },
+  {
+    id: "brittany",
+    name: "Brittany Newton",
+    role: "Community Engagement Coordinator",
+    link: "https://www.linkedin.com/in/brittany-newton-648452133/",
+    gradYear: 2020
+  },
+  {
+    id: "stella",
+    name: "Stella Seki",
+    role: "Community Engagement Coordinator",
+    link: "https://www.linkedin.com/in/stella-seki-043051170/",
+    gradYear: 2021
+  },
+  {
+    id: "nhu",
+    name: "Nhu Luong",
+    role: "Visual + Brand Designer",
+    link: "http://nhu777.net",
+    gradYear: 2021
+  },
+  {
+    id: "julia",
+    name: "Julia Nguyen",
+    role: "Visual + Brand Designer",
+    link: "https://nguyenjuliaa.com/",
+    gradYear: 2021
+  },
+  {
+    id: "alison",
+    name: "Alison Chen",
+    role: "Web Developer",
+    link: "https://www.linkedin.com/in/ealisonchen/",
+    gradYear: 2021
+  },
+  {
+    id: "paridhi",
+    name: "Paridhi Khaitan",
+    role: "Web Developer",
+    link: "https://www.linkedin.com/in/paridhi4699/",
+    gradYear: 2021
+  },
+  {
+    id: "vicky",
+    name: "Vicky Ly",
+    role: "Marketing Coordinator",
+    link: "https://www.linkedin.com/in/",
+    gradYear: 2021
+  },
+  {
+    id: "quinn",
+    name: "Quinn Vu",
+    role: "Marketing Coordinator",
+    link: "https://www.linkedin.com/in/thyvu1014/",
+    gradYear: 2021
+  },
+  {
+    id: "winson",
+    name: "Winson Dieu",
+    role: "Industry Relations Coordinator",
+    link: "https://www.linkedin.com/in/winson-dieu/",
+    gradYear: 2021
+  },
+  {
+    id: "andrew",
+    name: "Winson Dieu",
+    role: "Industry Relations Coordinator",
+    link: "https://www.linkedin.com/in/andrew-zhen/",
+    gradYear: 2021
+  },
+/*
+  {
+    id: "emily",
+    name: "Katya Noble",
+    role: "Director of Growth and Strategy",
+    link: "https://www.katyanoble.design/",
+    gradYear: 2019
+  },
+  
+  {
     id: "steven",
     name: "Steven Chen",
     role: "President",
